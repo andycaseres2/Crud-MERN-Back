498b635ee6bc4114567140ccd46d9eb0f519637f
Andy Caseres <andycaseres2@gmail.com>
2022-11-26 08:39:54 -0500

    corrections old

--- src/app.js
+++ src/app.js
@@ -2,34 +2,35 @@ import express from "express"
 import cors from "cors"
 import db from "./database/db.js"
 import personsRoutes from "./routes/routes.js"
-import mysql from "mysql2"
+//import mysql from "mysql2"
 
 const app = express()
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-})
+const PORT = process.env.PORT || 5000
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// })
 app.use(cors())
 app.use(express.json())
 app.use("/persons", personsRoutes)
-// ;async () => {
-//   try {
-//     await db.authenticate()
-//     console.log("Connection DB successfully")
-//   } catch (error) {
-//     console.log(`Error: ${error.message}`)
-//   }
-// }
-pool.getConnection((err, coon) => {
-  if (err) console.log(err)
-  console.log("Connection successfully")
-})
+;async () => {
+  try {
+    await db.authenticate()
+    console.log("Connection DB successfully")
+  } catch (error) {
+    console.log(`Error: ${error.message}`)
+  }
+}
+// pool.getConnection((err, coon) => {
+//   if (err) console.log(err)
+//   console.log("Connection successfully")
+// })
 app.get("/ping", (req, res) => {
   res.send("Hello World!")
 })
 
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server listening on port 5000 🚀`)
-// })
+app.listen(PORT, () => {
+  console.log(`🚀 Server listening on port ${PORT} 🚀`)
+})
